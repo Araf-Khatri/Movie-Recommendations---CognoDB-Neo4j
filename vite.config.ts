b54f3d9092f99ef4886eac916054b075e0ada2a2
Araf-Khatri <arafkhatri0@gmail.com>
2026-08-12 16:51:44 +0530

    fix: update server entry point and clean up package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import netlify from "@netlify/vite-plugin-tanstack-start";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tanstackStart(), netlify()],
+  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths(), netlify()],
 });
